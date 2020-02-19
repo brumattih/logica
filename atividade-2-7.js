db4e3959080243f9db72f4e39e4a29038eeda520
brumattih <hrpreis@gmail.com>
2020-02-19 17:18:23 -0300

    Atividade 2-7

---
+++ atividade-2-7.js
@@ -0,0 +1,15 @@
+// Elabore um programa para testar se uma senha digitada é igual a “batatafrita”. 
+// Se a senha estiver correta escreva 
+// 	“Acesso permitido”, 
+// do contrário emita a mensagem 
+// 	“Você não tem acesso ao sistema”.
+
+var user = require('readline-sync')
+
+
+var senha = user.question("Digite a senha: ")
+if (senha == 'batatafrita') {
+    console.log("Acesso permitido")
+} else {
+    console.log("Você não tem acesso ao sistema")
+}
