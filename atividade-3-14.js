b2fd9f311650a28940fba7b991e8d922269f5853
brumattih <hrpreis@gmail.com>
2020-02-19 00:48:14 -0300

    Exercício 3-14 (Necessário corrigir)

---
+++ atividade-3-14.js
@@ -0,0 +1,24 @@
+// Fazer um algoritmo que receba 8 idades do usuário e no final ele imprimir qual é a menor idade.
+
+
+var user = require('readline-sync')
+var n = 1
+
+while (n <= 8) {
+    var idade = user.questionInt("Digite oito idades: ")
+    var idadeAux = idade
+    var idadeAux2 = 50
+
+    if (idadeAux >= idadeAux2) {
+        idadeAux =                 
+    }
+    n++
+
+}
+
+if (idadeAux >= idadeAux2) {
+    console.log(`A MENORIDADEAUX2 idade é ${idadeAux2}`)
+} else {
+    console.log(`A MENOR é ${idadeAux}`)
+
+}
